444df4403673f20ae8129d572dec758c241b1910
georgefs <george.li@tagtoo.org>
2014-06-19 18:33:15 +0000

    update

--- log2bq.py
+++ log2bq.py
@@ -1,7 +1,11 @@
-from taskController import ExternalTaskController, TaskController
+from taskController2 import ExternalTaskController, TaskController
 import json
+from datetime import timedelta, datetime
+import luigi
 
 class Log2GsTask(TaskController):
+
+    
     _task_path = 'share_libs.pipelines.Log2Gs'
     _task_url = 'http://client.tagtootrack.appspot.com/'
     _kwargs = {
@@ -14,31 +18,65 @@ class Log2GsTask(TaskController):
         'shards': 5
     }
 
+    output_format = "{0.__class__.__name__}_{0.hour_time:%Y-%m-%dT%H}"
+    hour_time = luigi.DateHourParameter()
+
+
+    def prepare_args(self, inputs):
+        args = super(Log2GsTask, self).prepare_args(inputs)
+        start_time = self.hour_time.strftime('%s')
+        start_time = int(start_time)
+        end_time = start_time + 3600
+
+        
+        args['kwargs']['start_time'] = start_time
+        args['kwargs']['end_time'] = end_time
+        return args
+
+
 
 
 class Gs2BqTask(TaskController):
+
+
     _task_path = 'share_libs.pipelines.Gs2Bq'
     _task_url = 'http://client.tagtootrack.appspot.com/'
     _kwargs = {
-        #'files': ["/gs/tagtootest/test-1577677112291931E7B4A-output"],
+        #'files': ["/gs/tagtootest/test-1577677112291931E7B4A-output"],  save to bigquery file
         'bqproject': '103827621493',
         'bqdataset': 'tagtootest',
         'table': 'test_table',
         'fields': [{'type': 'string', 'name': 'features', 'mode': 'repeated'}, {'type': 'string', 'name': 'vars', 'mode': 'repeated'}, {'type': 'string', 'name': 'ip'}, {'type': 'string', 'name': 'session'}, {'type': 'string', 'name': 'slot'}, {'type': 'string', 'name': 'title'}, {'type': 'string', 'name': 'creative'}, {'type': 'string', 'name': 'pc'}, {'type': 'string', 'name': 'version'}, {'type': 'string', 'name': 'type'}, {'type': 'string', 'name': 'publisher'}, {'type': 'timestamp', 'name': 'start_time'}, {'type': 'string', 'name': 'host'}, {'type': 'string', 'name': 'user'}, {'type': 'string', 'name': 'qp'}, {'type': 'string', 'name': 'target'}, {'fields': [{'type': 'string', 'name': 'var'}, {'type': 'string', 'name': 'advertiser'}, {'type': 'string', 'name': 'campaign'}], 'type': 'record', 'name': 'items', 'mode': 'repeated'}, {'type': 'string', 'name': 'ext'}, {'type': 'string', 'name': 'tag'}, {'type': 'string', 'name': 'user_agent'}, {'type': 'string', 'name': 'referral'}, {'type': 'string', 'name': 'qm'}, {'type': 'string', 'name': 'page'}]
     }
 
-    def requires(self):
-        return [Log2GsTask()]
+    hour_time = luigi.DateHourParameter()
+    output_format = "{0.__class__.__name__}_{0.hour_time:%Y-%m-%dT%H}"
 
-    def pre_trigger(self):
-        files = json.loads(self._inputs[0])['output']['default']
-        self._kwargs['files'] = files
+    def requires(self):
+        return [Log2GsTask(self.hour_time)]
+
+    def prepare_args(self, inputs):
+        args = super(Gs2BqTask, self).prepare_args(inputs)
+
+        files = json.loads(inputs[0])['output']['default']
+        args['kwargs']['files'] = files
+        return args
         
 
+class GenReport(TaskController):
+    date = luigi.DateParameter(default=datetime(2014,06,19))
 
+    output_format = "{0.__class__.__name__}_{0.date:%Y-%m-%d}"
 
+    def requires(self):
+        return [Gs2BqTask(self.date + timedelta(hours=i)) for i in xrange(24)]
     
+    def prepare_args(self, inputs):
+        date = date.strftime('%Y-%m-%d')
+        args = super(Gs2BqTask, self).prepare_args()
+        args['kwargs']['date'] = date
+        return args
     
     
 import luigi
-luigi.run(main_task_cls=Gs2BqTask) 
+luigi.run(main_task_cls=GenReport()) 
--- taskController.py
+++ taskController.py
@@ -3,109 +3,146 @@ import luigi
 import datetime
 import json
 import time
+import logging
 import urllib2, urllib
 import urlparse
+logger = logging.getLogger('luigi-interface')
 
 TRIGGER = "/_task/trigger"
 STATUS = "/_task/status"
 STOP = "/_task/stop"
+
 
 # task server 端
 class TaskController(luigi.Task):
 
     _task_url = None #url which support taskWorker
     _task_path = None #path where taskWorker here 
-    _args = []
-    _kwargs = {}
+    _args = [] # for task args
+    _kwargs = {} # for task kwargs
     _delay = 30 # check status lantance
-    _input = []
-    _id = None # task id
+    _id = None # custom setting task id
+
+    _retry = 0 #retry times 
+    _timeout = 0 # time out for sec 0 for unlimit
+    
+    output_format = "{0.__class__.__name__}"
 
     def requires(self):
         return []
-    
 
     def output(self):
-        name = self.__class__.__name__
+        name = self.output_format.format(self)
         return luigi.LocalTarget(name)
 
     def run(self):
+        inputs = self.__read_input()
+
+        args = self.prepare_args(inputs)
+        task_id = self.__start_task(args)
+        
+        for retry_count in xrange(self._retry + 1):
+            try:
+                result = self.__watting_task(task_id)
+            except Exception as e:
+                self.__retry_task(task_id)
+            finally:
+                logger.info('retry {}'.format(retry_count))
+                if self._retry and retry_count >= self._retry:
+                    logger.error('maxmun retry')
+                    raise Exception('maxmun retry')
+
+        self.__write_output(result)
+
+
+
+    ## prepare task prams
+    def prepare_args(self, inputs):
+        data = {
+            "args": self._args,
+            "kwargs": self._kwargs,
+        }
+
+        return data
+    
+
+    ## load task 
+    def __read_input(self):
         inputs = []
         print 'run'
         for result in self.input(): 
             result = result.open('r').read()
             inputs.append(result)
-        print 'get depance'
-
-        self._inputs = inputs
-        print 'trigger'
-        self.pre_trigger()
-        self._id = self._trigger()
-
-        result = self._check_status()
-
-        f = self.output().open('w')
-        f.write(json.dumps(result))
-        f.close()
-
-
-    def pre_trigger(self):
-        pass
+        return inputs
 
     
-    def _trigger(self):
-        trigger_url = urlparse.urljoin(self._task_url, TRIGGER)
-        data = {
-            "path": self._task_path,
-            "args": self._args,
-            "kwargs": json.dumps(self._kwargs)
-        }
-        query = urllib.urlencode(data)
-        resp = urllib2.urlopen(trigger_url, query)
+    ## trigger task
+    def __start_task(self, args):
+        trigger_api = urlparse.urljoin(self._task_url, TRIGGER)
+        
+        args['args'] = json.dumps(args['args'])
+        args['kwargs'] = json.dumps(args['kwargs'])
+        args['path'] = self._task_path
 
-        _id = json.loads(resp.read())['id']
-        print "id" + _id
+        query = urllib.urlencode(args)
+        resp = urllib2.urlopen(trigger_api, query)
+        
+        task_id = json.loads(resp.read())['id']
+        logger.info('start: {}'.format(task_id))
+        return task_id
 
-        return _id
+    
+    ## watting for task end and get task result
+    def __watting_task(self, task_id):
+        check_status_api = urlparse.urljoin(self._task_url, STATUS)
 
-
-    def _check_status(self):
-        status_url = urlparse.urljoin(self._task_url, STATUS)
-        status_url += "?id={}".format(self._id)
+        start_time = datetime.datetime.now()
 
         while True:
-            print 'check status'
-            time.sleep(self._delay)
             try:
-                resp = urllib2.urlopen(status_url)
-                info = json.loads(resp.read())
-                # success
-                if info['status'].lower() == "done":
-                    print 'success'
-                    return info
-                # error
-                elif info['status'].lower() == "failed":
-                    raise Exception(info["msg"])
-                else:
-                    continue
-
+                result = urllib2.urlopen(check_status_api + "?id=" + task_id ).read()
+                if json.loads(result)['status'] == 'done':
+                    break
+                logger.info('watting {}'.format(task_id))
 
             except urllib2.URLError, e:
                 if e.code == 500:
-                    import pdb;pdb.set_trace()
+                    logger.error('server error ')
                     raise e
                 else:
                     continue
+            except Exception as e:
+                logger.error('error for {}'.format(e.message))
+                raise e
+
+            finally:
+                time.sleep(self._delay)
+                delta = datetime.datetime.now() - start_time
+                assert not self._timeout or delta.total_seconds() < self._timeout, 'Timeout Exception'
 
 
-    def _stop(self):
-        stop_url = urllib.urljoin(self._task_url, STATUS)
-        stop_url += "?id={}".format(self._id)
 
-        urllib2.urlopen(stop_url)
+    ## stop task
+    def __stop_task(self, task_id):
+        stop_api = urlparse.urljoin(self._task_url, STOP)
+        urllib2.urlopen(stop_api + "?id="+task_id).read()
+
+
+    ## retry task
+    def __retry_task(self, task_id):
+        logger.info('retry')
+        self.__stop_task(task_id)
+        self.__start_task()
+
+
+    def __write_output(self, result):
+        output_file = self.output.open('w')
+        output_file.write(json.dumps(result))
 
 
 
+
+        
         
 
 
